1e162e820fb229c7c4a1c22a168f06a9297bd74b
Vettiger, Linus <linus.vettiger@novartis.com>
2019-06-05 11:21:01 +0200

    Implemented dynamic fs data source

---
+++ api/reader.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const folder = './posts';
+
+/**
+ *
+ * @param slug Slug of the post to fetch
+ * @returns {*} The complete post
+ */
+const readPost = (slug) => {
+  try {
+    const content = fs.readFileSync(`${ folder }/${ slug }.md`, 'utf-8');
+    const meta = JSON.parse(fs.readFileSync(`${ folder }/${ slug }.meta.json`, 'utf-8'));
+
+    return {
+      ...meta,
+      slug,
+      content,
+    };
+  } catch {}
+};
+
+/**
+ * Get a list of all posts
+ * @returns {any[]}
+ */
+const listPosts = () => {
+  const files = fs.readdirSync(folder);
+  return files
+    .filter(file => file.endsWith('.md')) // filter for only markdown files
+    .map(f => f.substring(0, f.length - 3)) // Remove .md extension
+    .map(readPost); // Get all post contents
+};
+
+module.exports = {
+  readPost,
+  listPosts,
+};
